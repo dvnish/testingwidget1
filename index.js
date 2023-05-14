a54a783994c424c613f5e536ddd3eebbfa87d030
dvnish <cricket-kid6@Hotmail.com>
2023-05-14 04:50:51 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+// const container = document.getElementById('root');
+// const root = ReactDOM.createRoot(container);
+// root.render(<App />);
+
+// Find all widget divs
+//const container2 = document.querySelectorAll('#widgetx')
+//const WidgetDivs = createRoot(container2)
+
+
+const WidgetDivs = document.querySelectorAll('.idealy_widget');
+
+
+
+WidgetDivs.forEach(Div => {
+  const root = ReactDOM.createRoot(Div);
+  root.render(
+    <React.StrictMode>
+      <App domElement={Div}/>
+    </React.StrictMode>,
+    Div
+  );
+});
+  
+
+  
+  
+  
+
+
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
